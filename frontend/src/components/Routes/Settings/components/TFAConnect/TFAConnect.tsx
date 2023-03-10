b39700d1181ab9e9cec6b7ba5d1bbbc3556bc440
ArmVirabyan <armanvirabyan@gmail.com>
2023-03-10 15:07:09 +0400

    Feat: TFA popup

--- frontend/src/components/Routes/Settings/components/TFAConnect/TFAConnect.tsx
+++ frontend/src/components/Routes/Settings/components/TFAConnect/TFAConnect.tsx
@@ -1,18 +1,32 @@
 import { Card } from 'antd';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { InfoCircleTwoTone } from '@ant-design/icons';
 
+import TFAPopup from './TFAPopup/TFAPopup';
 import classes from './TFAConnect.module.scss';
 import qrImage from './qr-example.png';
 // TODO
 
 const TFAConnect: FC = () => {
   const { t } = useTranslation();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
   return (
     <Card
       className={classes.root}
       title={t('Connect Google Two Factor Authentication')}>
+      <div className={classes.infobutton}>
+        <InfoCircleTwoTone
+          type='primary'
+          onClick={showModal}
+          style={{ fontSize: 24 }}
+        />
+      </div>
+      <TFAPopup isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
       <div className={classes.qrWrapper}>
         <img src={qrImage} alt='qr' className={classes.qrImage} />
       </div>
---
+++ frontend/src/components/Routes/Settings/components/TFAConnect/TFAPopup/TFAPopup.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Modal } from 'antd';
+import { useTranslation } from 'react-i18next';
+
+type PropTypes = {
+  isModalOpen: boolean;
+  setIsModalOpen: (isModalOpen: boolean) => void;
+};
+const App = ({ isModalOpen, setIsModalOpen }: PropTypes) => {
+  const { t } = useTranslation();
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <Modal
+      footer={null}
+      title={t('Google Two Factor Authentication')}
+      open={isModalOpen}
+      onCancel={handleCancel}>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla aliquid
+        eum vitae quo nostrum necessitatibus. Obcaecati nesciunt magnam hic sit
+        aliquam ut, minus quas quis assumenda esse. Minus atque reprehenderit
+        numquam doloribus ducimus vitae aut necessitatibus, repudiandae
+        obcaecati, nesciunt incidunt eveniet. Deleniti quo consequatur
+        voluptatibus odit ratione et magnam. Ex quibusdam facilis tenetur id
+        aperiam saepe. Inventore, alias. Odio quis rem dignissimos in
+        laudantium. Reprehenderit nihil quibusdam quo saepe esse quam cum,
+        dignissimos quia quod, inventore explicabo natus illo fugiat, error
+        adipisci incidunt ea eum ullam aspernatur ex? Tempora quae architecto
+        dignissimos quas officiis dicta excepturi facere velit id repellendus.
+      </p>
+    </Modal>
+  );
+};
+
+export default App;
