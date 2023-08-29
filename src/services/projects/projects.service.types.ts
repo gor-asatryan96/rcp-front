c1cbbc20a4e48bc71a143d8d99c50f1d3839cf34
ArmVirabyan <armanvirabyan@gmail.com>
2023-08-29 12:35:32 +0400

    Manual IN save pop-up

--- src/services/projects/projects.service.types.ts
+++ src/services/projects/projects.service.types.ts
@@ -8,3 +8,24 @@ export interface IProjectResponse extends IProjectGeneralLimits {
   project: string;
   project_db: string;
 }
+
+export interface IProjectInfo {
+  approver_test_mode: 0 | 1;
+  casino_ggr_limit: string;
+  casino_winning_limit: string;
+  daily_withdraw_limit: string;
+  games_ggr_limit: string;
+  games_winning_limit: string;
+  id: number;
+  is_active: 0 | 1;
+  manual_push_route: string;
+  mi_limit: string;
+  project: string;
+  project_api_key: string;
+  project_db: string;
+  project_tz: string;
+  rollback_limit_percentage: string;
+  sport_ggr_limit: string;
+  sport_winning_limit: string;
+  used_unused_percentage: string;
+}
